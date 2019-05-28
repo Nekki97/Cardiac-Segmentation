b37eb752ec5f2c0fe7bd5bee28c125465b702241
Nekki97 <43118852+Nekki97@users.noreply.github.com>
2019-05-28 22:08:08 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -4,141 +4,161 @@ import random
 import imageio
 from sklearn import model_selection
 
-def save_datavisualisation3(img_data, myocar_labels, predicted_labels, save_folder, index_first = False, normalized = False):
-    img_data_temp = []
-    myocar_labels_temp = []
-    predicted_labels_temp = []
-    if index_first == True:
-        for i in range(0, len(img_data)):
-            img_data_temp.append(np.moveaxis(img_data[i], 0, -1))
-            myocar_labels_temp.append(np.moveaxis(myocar_labels[i], 0, -1))
-            predicted_labels_temp.append(np.moveaxis(predicted_labels[i], 0, -1))
-    counter = 0
-    for i, j, k in zip(img_data_temp[:], myocar_labels_temp[:], predicted_labels_temp[:]):
-        print(counter)
-        print(i.shape)
-        i_patch = i[:, :, 0]
-        if normalized == True:
-            i_patch = i_patch*255
-        # np.squeeze(i_patch)
-
-        j_patch = j[:, :, 0]
-        # np.squeeze(j_patch)
-        j_patch = j_patch * 255
-
-        k_patch = k[:,:,0]
-        k_patch = k_patch*255
-
-        for slice in range(1, i.shape[2]):
-            temp = i[:, :, slice]
-            # np.squeeze(temp)
-            if normalized == True:
-                temp = temp * 255
-            i_patch = np.hstack((i_patch, temp))
-
-
-            temp = j[:, :, slice]
-            # np.squeeze(temp)
-            temp = temp * 255
-            j_patch = np.hstack((j_patch, temp))
-
-            temp = k[:,:,slice]
-            temp = temp*255
-            k_patch = np.hstack((k_patch, temp))
-
-        image = np.vstack((i_patch, j_patch, k_patch))
-
-        print(image.shape)
-        imageio.imwrite(save_folder + '%d.png' % (counter,), image)
-        counter = counter + 1
-
-'''
-plt.imshow(test_images[0][:,:,0], plt.cm.gray)
-plt.show()
-plt.imshow(test_masks[0], plt.cm.gray)
-plt.show()
-plt.imshow(results[0][:,:,0], plt.cm.gray)
-plt.show()
-
-# Plot training & validation accuracy values
-plt.plot(history.history['acc'])
-plt.plot(history.history['val_acc'])
-plt.title('Model accuracy')
-plt.ylabel('Accuracy')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Test'], loc='upper left')
-plt.show()
-
-# Plot training & validation loss values
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('Model loss')
-plt.ylabel('Loss')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Test'], loc='upper left')
-plt.show()
-
-'''
-
-def split(images, masks, perc):
-    '''
-    :param perc: percentage of total data that are turned into test-dataset
-    '''
-    imagecount = len(images)
-    print(imagecount)
-
-    seed = 698869
-    np.random.seed(seed)
-    np.random.shuffle(images)
-    np.random.shuffle(masks)
-
-    testdata_count = int(imagecount * perc)
-    traindata_count = imagecount - testdata_count
-
-    train_images = []
-    test_images = []
-    train_masks = []
-    test_masks = []
-    for image in images[:traindata_count]:
-        train_images.append(image)
-    for mask in masks[:traindata_count]:
-        train_masks.append(mask)
-    for image in images[traindata_count:traindata_count+testdata_count]:
-        test_images.append(image)
-    for mask in masks[traindata_count:traindata_count+testdata_count]:
-        test_masks.append(mask)
-    print(len(train_images))
-    print(len(train_masks))
-    print(len(test_images))
-    print(len(test_masks))
-
-    return train_images, train_masks, test_images, test_masks
+def save_datavisualisation3(img_data, myocar_labels, predited_labels, save_folder, name, bunch_size, rows, percentage, normalized = False):
+    amount = int(img_data.shape[0] * percentage)
+    ind = random.sample(range(img_data.shape[0]), amount)
+    img_data = img_data[ind]
+    myocar_labels = myocar_labels[ind]
+    predited_labels = predited_labels[ind]
+    pics = int(img_data.shape[0] / (bunch_size * rows)) + 1
+    remaining_imgs = img_data.shape[0]
+    for pic in range(pics):
+        indx_start = bunch_size * rows * pic
+        indx_end = bunch_size * rows * (pic + 1)
+        i_bunch = []
+        j_bunch = []
+        k_bunch = []
+        while len(i_bunch) < bunch_size * rows and len(i_bunch) < img_data.shape[0]:
+            while len(j_bunch) < bunch_size * rows and len(j_bunch) < myocar_labels.shape[0]:
+                while len(k_bunch) < bunch_size * rows and len(k_bunch) < predited_labels.shape[0]:
+                    for i, j, k in zip(img_data[indx_start:indx_end, :, :, 0], myocar_labels[indx_start:indx_end, :, :, 0], predited_labels[indx_start:indx_end, :, :, 0]):
+                        if normalized == True:
+                            i = i * 255
+                            j = j * 255
+                            k = k * 255
+                        i_bunch.append(i)
+                        j_bunch.append(j)
+                        k_bunch.append(k)
+        i_bunch = np.array(i_bunch, dtype=float)
+        j_bunch = np.array(j_bunch, dtype=float)
+        k_bunch = np.array(k_bunch, dtype=float)
+        full_rows = []
+        for row_number in range(rows):
+            if remaining_imgs > 0:
+                i_row = i_bunch[bunch_size * row_number, :, :]
+                j_row = j_bunch[bunch_size * row_number, :, :]
+                k_row = k_bunch[bunch_size * row_number, :, :]
+                remaining_imgs = max(remaining_imgs - 1, 0)
+                for index in range(bunch_size * row_number + 1,bunch_size * (row_number + 1)):
+                    if remaining_imgs > 0:
+                        image = i_bunch[index, :, :]
+                        myocar_label = j_bunch[index, :, :]
+                        predicted_label = k_bunch[index, :, :]
+                    else:
+                        x = img_data.shape[1]
+                        y = img_data.shape[2]
+                        image = np.zeros((x, y))
+                        myocar_label = np.zeros((x, y))
+                        predicted_label = np.zeros((x,y))
+                    i_row = np.hstack((i_row, image))
+                    j_row = np.hstack((j_row, myocar_label))
+                    k_row = np.hstack((k_row, predicted_label))
+                    remaining_imgs = max(remaining_imgs - 1, 0)
+                full_row = np.vstack((i_row, j_row, k_row))
+                if row_number == 0:
+                    full_rows = full_row
+                else:
+                    full_rows = np.array(full_rows, dtype=float)
+                    full_rows = np.append(full_rows, full_row, axis=0)
+        full_image = full_rows[0]
+        for full_row in full_rows[1:full_rows.shape[0]]:
+            full_image = np.vstack((full_image, full_row))
+        imageio.imwrite(save_folder + "/" + name + '%d_%d.png' % (indx_start, indx_end-1), full_image)
+        print("Visualised a bunch")
+    print("******Finished visualising " + str(amount) + " images******")
 
 
-def get_splits(images, masks, splits):
-    # splits in form of {1: (0.2,0.2), 2: (0.3,0.4)} (die Erste ist train-test, die Zweite train-val)
-    arr_train_images = []
-    arr_train_masks = []
-    arr_test_images = []
-    arr_test_masks = []
-    arr_val_images = []
-    arr_val_masks = []
+def save_datavisualisation2(img_data, myocar_labels, save_folder, name, bunch_size, rows, percentage, normalized = False):
+    amount = int(img_data.shape[0] * percentage)
+    ind = random.sample(range(img_data.shape[0]), amount)
+    img_data = img_data[ind]
+    myocar_labels = myocar_labels[ind]
+    pics = int(img_data.shape[0]/(bunch_size*rows))+1
+    remaining_imgs = img_data.shape[0]
+    for pic in range(pics):
+        indx_start = bunch_size * rows * pic
+        indx_end = bunch_size * rows * (pic + 1)
+        i_bunch = []
+        j_bunch = []
+        while len(i_bunch) < bunch_size * rows and len(i_bunch) <= img_data.shape[0]:
+            while len(j_bunch) < bunch_size * rows and len(j_bunch) <= myocar_labels.shape[0]:
+                for i, j in zip(img_data[indx_start:indx_end,:,:,0], myocar_labels[indx_start:indx_end,:,:,0]):
+                    if normalized == True:
+                        i = i * 255
+                        j = j * 255
+                    j_bunch.append(j)
+                    i_bunch.append(i)
+        i_bunch = np.array(i_bunch, dtype=float)
+        j_bunch = np.array(j_bunch, dtype=float)
+        full_rows = []
+        for row_number in range(rows):
+            if remaining_imgs > 0:
+                i_row = i_bunch[bunch_size*row_number, :, :]
+                j_row = j_bunch[bunch_size*row_number, :, :]
+                remaining_imgs = max(remaining_imgs - 1, 0)
+                for index in range(bunch_size*row_number+1, bunch_size*(row_number+1)):
+                    if remaining_imgs > 0:
+                        image = i_bunch[index, :, :]
+                        label = j_bunch[index, :, :]
+                    else:
+                        x = img_data.shape[1]
+                        y = img_data.shape[2]
+                        image = np.zeros((x,y))
+                        label = np.zeros((x,y))
+                    i_row = np.hstack((i_row, image))
+                    j_row = np.hstack((j_row, label))
+                    remaining_imgs = max(remaining_imgs - 1, 0)
+                full_row = np.vstack((i_row, j_row))
+                if row_number == 0:
+                    full_rows = full_row
+                else:
+                    full_rows = np.array(full_rows, dtype=float)
+                    full_rows = np.append(full_rows, full_row, axis=0)
+        full_image = full_rows[0]
+        for full_row in full_rows[1:full_rows.shape[0]]:
+            full_image = np.vstack((full_image, full_row))
+        imageio.imwrite(save_folder + "/" + name + '%d_%d.png' % (indx_start,indx_end-1), full_image)
+    print("******Finished visualising " + str(amount) + " images******")
 
-    for split in splits.values():     #TODO Look at it
 
-        train_val_images, test_images, train_val_masks, test_masks = \
+
+def get_split(images, masks, split):
+    # split in form of (0.2,0.2)
+
+    train_val_images, test_images, train_val_masks, test_masks = \
             model_selection.train_test_split(images, masks, test_size=split[0])
 
-        train_images, val_images, train_masks, val_masks = \
+    train_images, val_images, train_masks, val_masks = \
             model_selection.train_test_split(train_val_images, train_val_masks, test_size=split[1])
 
-        print(len(train_images))
-        print(train_images.shape())
-        np.concatenate(arr_train_images, train_images)
-        np.concatenate(arr_train_masks, train_masks)
-        np.concatenate(arr_test_images, test_images)
-        np.concatenate(arr_test_masks, test_masks)
-        np.concatenate(arr_val_images, val_images)
-        np.concatenate(arr_val_masks, val_masks)
+    return train_images, train_masks, val_images, val_masks, test_images, test_masks
 
-    return arr_train_images, arr_train_masks, arr_val_images, arr_val_masks, arr_test_images, arr_test_masks
+
+def get_splits(images, masks,splits):
+    # split in form of {1:(0.2, 0.2), 2:(0.3, 0.4)}
+    # return: {split1: {train_images_split1: (100,96,96), train_masks_split1: ... }, split2: {}} for every split
+    split_dicts = {}
+    j = 0
+    for split in splits.values():
+        split_data = {}
+        labels = ["train_images", "train_masks", "val_images", "val_masks",
+                "test_images", "test_masks"]
+        train_images, train_masks, val_images, val_masks, test_images, test_masks = get_split(images, masks, split)
+        data = [train_images, train_masks, val_images, val_masks, test_images, test_masks]
+        for i in range(len(data)):
+            split_data[labels[i]] = data[i]
+        split_dicts[j] = split_data
+        j += 1
+    return split_dicts
+
+
+def get_datasets(data, split_number):
+
+    train_images = data[split_number].get("train_images")
+    train_masks = data[split_number].get("train_masks")
+    val_images = data[split_number].get("val_images")
+    val_masks = data[split_number].get("val_masks")
+    test_images = data[split_number].get("test_images")
+    test_masks = data[split_number].get("test_masks")
+    
+    return train_images, train_masks, val_images, val_masks, test_images, test_masks
