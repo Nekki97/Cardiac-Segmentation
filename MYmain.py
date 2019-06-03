408dfec77568d6724fa5f8e060cb6e55339b8a5c
Nekki97 <43118852+Nekki97@users.noreply.github.com>
2019-06-03 02:42:01 +0200

    Update MYmain.py

--- MYmain.py
+++ MYmain.py
@@ -1,36 +1,37 @@
 from keras.models import *
 from keras.callbacks import ModelCheckpoint
-from MYmodel import *
+from MYmodel import param_unet
 import PGM_Loader_new as pgm
-import os
 from functions import *
 import scoring_utils as su
-#import torch
-#from sklearn.metrics import roc_curve, auc
+from matplotlib import pyplot as plt
+# import Nii_Loader as nii
+# import torch
+# from sklearn.metrics import roc_curve, auc
 
-epochs = 100
+epochs = 1
 batch_size = 32
-filters = 64
+filters = 8
 dropout_rate = 0.5
 cropper_size = 64 # --> 128x128 after padding
 
 whichmodel = "param_unet"
-seeds = [1, 2, 3, 4]
-data_percs = [0.3, 0.5, 0.8, 1]
-layers_arr = [3, 5, 6, 7] # 1 layer = 2 Convolutions, 1 MaxPooling, 1 Dropout (technically 4 layers)
-splits = {1: (0.2, 0.2), 2: (0.3, 0.3), 3: (0.2, 0.3), 4: (0.1, 0.1)}
+seeds = [1, 2]
+data_percs = [0.2, 0.3]
+layers_arr = [3, 4]
+splits = {1: (0.2, 0.2)}
 
 all_results = []
 
-(images, masks) = pgm.get_data(cropper_size)
-images = np.array(images)
-masks = np.array(masks)
+(pgm_images, pgm_masks) = pgm.get_data(cropper_size)
+pgm_images = np.array(pgm_images)
+pgm_masks = np.array(pgm_masks)
 
 for seed in seeds:
     for layers in layers_arr:
         for perc_index in range(len(data_percs)):
             for split_number in range(len(splits)):
-                data_dict = getalldata(images, masks, data_percs, splits, seed)
+                data_dict = getalldata(pgm_images, pgm_masks, data_percs, splits, seed)
                 data = data_dict[str(data_percs[perc_index])+"Perc"]
                 train_images, train_masks, val_images, val_masks, test_images, test_masks = \
                     get_datasets(data, split_number)
@@ -76,27 +77,35 @@ for seed in seeds:
                 else:
                     while os.path.exists(path + '/' + str(index)):
                         index += 1
+                    os.makedirs(path + '/' + str(index))
                     save_dir = path + '/' + str(index)
+                #if not os.path.exists(save_dir + '/checkpoints'):
+                #    os.makedirs(save_dir + '/checkpoints')
 
                 model = param_unet(input_size, filters, layers, dropout_rate)
-                #model_checkpoint = ModelCheckpoint(save_dir + '/checkpoints/unet{epoch:02d}.hdf5', monitor='loss',verbose=0, save_best_only=True)
-                history = model.fit(train_images, train_masks, epochs=epochs, batch_size=batch_size, validation_data=(val_images, val_masks), verbose=1, shuffle=True)#, callbacks=[model_checkpoint])
+                #model_checkpoint = ModelCheckpoint(save_dir + '/checkpoints/unet{epoch:02d}.hdf5', monitor='loss',
+                                                   #verbose=0, save_best_only=True)
+                history = model.fit(train_images, train_masks, epochs=epochs, batch_size=batch_size,
+                                    validation_data=(val_images, val_masks), verbose=1, shuffle=True)#,
+                                    #callbacks=[model_checkpoint])
 
                 results = model.predict(test_images, verbose=1)
-                np.save("data" ,results)
+                np.save("data", results)
+
 
                 '''
                 save_datavisualisation2(train_images, train_masks, save_dir, "train", 12, 3, 0.2, True)
                 save_datavisualisation2(test_images, test_masks, save_dir, "test", 12, 3, 0.2, True)
                 save_datavisualisation2(val_images, val_masks, save_dir, "val", 12, 3, 0.2, True)
 
-                plt.imshow(test_images[0][:,:,0], cmap='gray')
+                plt.imshow(test_images[0][:, :, 0], cmap='gray')
                 plt.show()
-                plt.imshow(test_masks[0][:,:,0], cmap='gray')
+                plt.imshow(test_masks[0][:, :, 0], cmap='gray')
                 plt.show()
-                plt.imshow(results[0][:,:,0], cmap='gray')
+                plt.imshow(results[0][:, :, 0], cmap='gray')
                 plt.show()
                 '''
+
 
                 # Plot training & validation accuracy values
                 plt.plot(history.history['acc'])
@@ -105,8 +114,9 @@ for seed in seeds:
                 plt.ylabel('Accuracy')
                 plt.xlabel('Epoch')
                 plt.legend(['Train', 'Test'], loc='upper left')
-                plt.savefig(os.path.join(save_dir, str(epochs) +'epochs_accuracy_values.png'))
+                plt.savefig(os.path.join(save_dir, str(epochs) + 'epochs_accuracy_values.png'))
                 #plt.show()
+
                 plt.close()
 
                 # Plot training & validation loss values
@@ -116,8 +126,9 @@ for seed in seeds:
                 plt.ylabel('Loss')
                 plt.xlabel('Epoch')
                 plt.legend(['Train', 'Test'], loc='upper left')
-                plt.savefig(os.path.join(save_dir, str(epochs) +'epochs_loss_values.png'))
+                plt.savefig(os.path.join(save_dir, str(epochs) + 'epochs_loss_values.png'))
                 #plt.show()
+
                 plt.close()
 
                 mask_prediction = []
@@ -131,32 +142,36 @@ for seed in seeds:
 
                 results = {
                     "epochs": epochs,
-                    "dice": "dice",
+                    #"dice": "dice",
                     "median_dice_score": "median_dice_score",
                     "train_val_test_split": train_val_test_split,
                     "unet_layers": layers,
                     "data_perc": data_percs[perc_index],
+                    "seed": seed,
                     "filters": filters,
-                    "input_size": input_size,
-                    "seed": seed
+                    "input_size": input_size
                 }
                 dice = []
 
                 output = np.squeeze(mask_prediction)
                 test_masks_temp = np.squeeze(test_masks)
                 for s in range(test_masks.shape[0]):
-                    dice.append(su.dice(output[s,:], test_masks_temp[s,:]))
+                    dice.append(getdicescore(output[s, :], test_masks_temp[s, :]))
 
                 median_dice_score = np.median(dice)
 
                 results["median_dice_score"] = median_dice_score
-                results["dice"] = dice
+                #results["dice"] = dice
 
-                #torch.save(results, os.path.join(save_dir, str(epochs) + 'epochs_evaluation_results'))
+                #torch.save(
+                #results, os.path.join(save_dir, str(epochs) + 'epochs_evaluation_results'))
+                results_file = open(save_dir + "/results.txt", "w+")
+                results_file.write(str(results))
+                results_file.close()
 
-                #TODO: torch library doesnt work yet
+                # TODO: torch library doesnt work yet
 
-                print('DICE SCORE: ' + str(median_dice_score))
+                print('DICE SCORE: ' + str(round(median_dice_score, 2)))
 
                 output = np.expand_dims(output, -1)
                 #save_datavisualisation3(test_images, test_masks, output, save_dir, "prediction", 12, 2, 0.4, True)
@@ -171,3 +186,11 @@ print(' BEST MEDIAN DICE SCORE:', round(all_results[best_idx]["median_dice_score
       'split with seed:', all_results[best_idx]["seed"],
       'and', all_results[best_idx]["data_perc"]*100,
       '% of the data after', all_results[best_idx]["epochs"], "epochs")
+
+final_results_file = open("results/bestresults.txt", "w+")
+final_results_file.write(' BEST MEDIAN DICE SCORE: ' + str(round(all_results[best_idx]["median_dice_score"],2)) +
+                         ' with ' + str(all_results[best_idx]["train_val_test_split"]) +
+                         ' split with seed: ' + str(all_results[best_idx]["seed"]) +
+                         ' and ' + str(all_results[best_idx]["data_perc"]*100) +
+                         '% of the data after ' + str(all_results[best_idx]["epochs"]) + " epochs")
+final_results_file.close()
