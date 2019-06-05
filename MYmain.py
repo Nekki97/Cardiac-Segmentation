0877534edd9ac85b4e6cd62464b67960fe9535bb
Nekki97 <43118852+Nekki97@users.noreply.github.com>
2019-06-05 03:41:08 +0200

    Update MYmain.py

--- MYmain.py
+++ MYmain.py
@@ -20,9 +20,9 @@ splits = {1: (0.3, 0.1)}
 
 epochs = 100
 basic_batch_size = 32 # auf 0.75*self ab 5 layers
-seeds = [3]
+seeds = [1, 2, 3]
 data_percs = [0.75, 1] # PERCENTAGE OF PEOPLE (TOTAL DATA)
-layers_arr = [4]
+layers_arr = [2, 5]
 loss_funcs = ['binary crossentropy']
 patient_percs = [0.75, 1]
 
