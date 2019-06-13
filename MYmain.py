5d71689ffbbe0d94fcf72bcbc28055cb015c0ac1
Nekki97 <43118852+Nekki97@users.noreply.github.com>
2019-06-13 12:00:58 +0200

    Update MYmain.py

--- MYmain.py
+++ MYmain.py
@@ -8,7 +8,7 @@ import scoring_utils as su
 from matplotlib import pyplot as plt
 import pandas as pd
 
-#os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 import keras
 from keras.backend.tensorflow_backend import set_session
@@ -25,7 +25,7 @@ K.set_session(sess)
 # import torch
 from sklearn.metrics import roc_curve, auc
 
-#from tensorflow.python.client import device_lib
+from tensorflow.python.client import device_lib
 
 #print(device_lib.list_local_devices())
 
@@ -42,11 +42,11 @@ def reset_keras():
     except:
         pass
 
-    print(gc.collect()) # if it's done something you should see a number being outputted
+    #print(gc.collect()) # if it's done something you should see a number being outputted
 
     # use the same config as you used to create the session
     config = tf.ConfigProto()
-    config.gpu_options.allow_growth = True
+    #config.gpu_options.allow_growth = True
     config.gpu_options.per_process_gpu_memory_fraction = 1
     config.gpu_options.visible_device_list = "0"
     set_session(tf.Session(config=config))
@@ -55,160 +55,175 @@ filters = 64
 dropout_rate = 0.5
 cropper_size = 64 # --> 128x128 after padding
 
-whichmodel = 'param_unet'
-#whichmodel = 'segnet'
+whichmodels = ['param_unet']
 splits = {1:(0.3,0.1)}
 
 epochs = 100
-basic_batch_size = 32
-seeds = [1, 2, 3, 4]
+basic_batch_size = 24
+seeds = [5, 6, 7, 8]
 data_percs = [1, 0.75, 0.5, 0.25] # PERCENTAGE OF PEOPLE (TOTAL DATA)
-layers_arr = [7, 6, 5]
-loss_funcs = ["weighted_cross_entropy", "binary_crossentropy", "dice"]
-patient_percs = [1]
+layers_arr = [5, 4, 3, 2]
+loss_funcs = ["binary_crossentropy"]
+patient_percs = [1, 0.75, 0.5]
 
 all_results = []
 
-for split in splits:
-    for loss_func in loss_funcs:
-        for layers in layers_arr:
-            if layers > 4:
-                batch_size = int((0.5**(layers-4))*basic_batch_size)
-            else:
-                batch_size = basic_batch_size
-            for perc in data_percs:
-                for patient_perc in patient_percs:
-                    for seed in seeds:
-                        reset_keras()
-                        random.seed(seed)
-                        np.random.seed(seed)
-                        tf.set_random_seed(seed)
-                        os.environ['PYTHONHASHSEED'] = str(seed)
+for whichmodel in whichmodels:
+    for split in splits:
+        for loss_func in loss_funcs:
+            for patient_perc in patient_percs:
+                for layers in layers_arr:
+                    if layers > 4:
+                        batch_size = int((0.5**(layers-4))*basic_batch_size)
+                    else:
+                        batch_size = basic_batch_size
+                    for perc in data_percs:
+                        for seed in seeds:
+                            reset_keras()
+                            random.seed(seed)
+                            np.random.seed(seed)
+                            tf.set_random_seed(seed)
+                            os.environ['PYTHONHASHSEED'] = str(seed)
 
+                            (pgm_images, pgm_masks) = pgm.get_data(cropper_size)
 
-                        (pgm_images, pgm_masks) = pgm.get_data(cropper_size)
+                            train_pats, test_pats, val_pats = get_patient_split(len(pgm_images), splits.get(split))
 
-                        train_pats, test_pats, val_pats = get_patient_split(len(pgm_images), splits.get(split))
+                            train_pats = get_total_perc_pats(train_pats, perc)
+                            test_pats = get_total_perc_pats(test_pats, 1)
+                            val_pats = get_total_perc_pats(val_pats, 1)
 
-                        train_pats = get_total_perc_pats(train_pats, perc)
-                        test_pats = get_total_perc_pats(test_pats, 1)
-                        val_pats = get_total_perc_pats(val_pats, 1)
+                            train_images, train_masks = get_patient_perc_split(pgm_images, pgm_masks, train_pats, patient_perc, False)
+                            test_images, test_masks = get_patient_perc_split(pgm_images, pgm_masks, test_pats, 1, True)
+                            val_images, val_masks = get_patient_perc_split(pgm_images, pgm_masks, val_pats, 1, False)
 
-                        train_images, train_masks = get_patient_perc_split(pgm_images, pgm_masks, train_pats, patient_perc, False)
-                        test_images, test_masks = get_patient_perc_split(pgm_images, pgm_masks, test_pats, 1, True)
-                        val_images, val_masks = get_patient_perc_split(pgm_images, pgm_masks, val_pats, 1, False)
+                            input_size = train_images.shape[1:4]
 
-                        input_size = train_images.shape[1:4]
+                            print("****************************************************************************************************************")
+                            print("EXPERIMENT:", layers, "layers,", perc*100, "% total data,", patient_perc*100, "% per pat,", loss_func, "loss function,", "seed", seed)
+                            print("TRAIN DATA SIZE", train_images.shape[0])
+                            print("VALIDATION DATA SIZE", val_images.shape[0])
+                            print("TEST DATA SIZE", sum([len(test_images)*test_images[i].shape[0] for i in range(len(test_images))]))
+                            print("****************************************************************************************************************")
 
-                        print("****************************************************************************************************************")
-                        print("EXPERIMENT:", layers, "layers,", perc*100, "% total data,", patient_perc*100, "% per pat,", loss_func, "loss function,", "seed", seed)
-                        print("TRAIN DATA SIZE", train_images.shape[0])
-                        print("VALIDATION DATA SIZE", val_images.shape[0])
-                        print("TEST DATA SIZE", test_images.shape[0])
-                        print("****************************************************************************************************************")
+                            index = 1
+                            path = "results/new/" + whichmodel + '-' + str(epochs) + "_epochs" + '-' + loss_func + '-' + \
+                                   str(int(perc*100)) + '%_total_data' + '-' + \
+                                   str(int(patient_perc*100)) + '%_per_pat' + '-' + str(splits.get(split)) + '_split' \
+                                   + '-' + str(layers) + '_layers' + '-' + 'seed_' + str(seed)
+                            if not os.path.exists(path + "-" + str(index) + '/'):
+                                os.makedirs(path + '-' + str(index) + '/')
+                                save_dir = path + '-' + str(index) + '/'
+                            else:
+                                while os.path.exists(path + '-' + str(index) + '/'):
+                                    index += 1
+                                os.makedirs(path + '-' + str(index) + '/')
+                                save_dir = path + '-' + str(index) + '/'
 
-                        index = 1
-                        path = "results/new/" + whichmodel + '-' + str(epochs) + "_epochs" + '-' + loss_func + '-' + \
-                               str(int(perc*100)) + '%_total_data' + '-' + \
-                               str(int(patient_perc*100)) + '%_per_pat' + '-' + str(splits.get(split)) + '_split' \
-                               + '-' + str(layers) + '_layers' + '-' + 'seed_' + str(seed)
-                        if not os.path.exists(path + "-" + str(index) + '/'):
-                            os.makedirs(path + '-' + str(index) + '/')
-                            save_dir = path + '-' + str(index) + '/'
-                        else:
-                            while os.path.exists(path + '-' + str(index) + '/'):
-                                index += 1
-                            os.makedirs(path + '-' + str(index) + '/')
-                            save_dir = path + '-' + str(index) + '/'
+                            if whichmodel == "param_unet":
+                                model = param_unet(input_size, filters, layers, dropout_rate, loss_func)
+                            if whichmodel == "segnet":
+                                model = segnet(input_size, 3, dropout_rate, loss_func)
+                            if whichmodel == 'DenseUNet':
+                                model = DenseUNet(input_size, loss_func)
 
-                        if whichmodel == "param_unet":
-                            model = param_unet(input_size, filters, layers, dropout_rate, loss_func)
-                        if whichmodel == "segnet":
-                            model = segnet(input_size, 3, dropout_rate, loss_func)
+                            es = keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0.0, patience=30, verbose=1,
+                                                               mode='min', baseline=0.5, restore_best_weights=False)
 
-                        es = keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0.0, patience=20, verbose=1,
-                                                           mode='min', baseline=0.3, restore_best_weights=False)
+                            history = model.fit(train_images, train_masks, epochs=epochs, batch_size=batch_size,
+                                                validation_data=(val_images, val_masks), verbose=2,
+                                                shuffle=True, callbacks=[es])
 
-                        history = model.fit(train_images, train_masks, epochs=epochs, batch_size=batch_size,
-                                            validation_data=(val_images, val_masks), verbose=2,
-                                            shuffle=True, callbacks=[es])
+                            results = model.predict(test_images, verbose=0)
+                            np.save("data", results)
 
-                        results = model.predict(test_images, verbose=0)
-                        np.save("data", results)
+                            # Plot training & validation accuracy values
 
-                        # Plot training & validation accuracy values
+                            plt.plot(history.history['acc'])
+                            plt.plot(history.history['val_acc'])
+                            plt.title('Model accuracy')
+                            plt.ylabel('Accuracy')
+                            plt.xlabel('Epoch')
+                            plt.legend(['Train', 'Test'], loc='upper left')
+                            plt.savefig(os.path.join(save_dir, str(epochs) + 'epochs_accuracy_values.png'))
+                            # plt.show()
 
-                        plt.plot(history.history['acc'])
-                        plt.plot(history.history['val_acc'])
-                        plt.title('Model accuracy')
-                        plt.ylabel('Accuracy')
-                        plt.xlabel('Epoch')
-                        plt.legend(['Train', 'Test'], loc='upper left')
-                        plt.savefig(os.path.join(save_dir, str(epochs) + 'epochs_accuracy_values.png'))
-                        # plt.show()
+                            plt.close()
 
-                        plt.close()
+                            # Plot training & validation loss values
+                            plt.plot(history.history['loss'])
+                            plt.plot(history.history['val_loss'])
+                            plt.title('Model loss')
+                            plt.ylabel('Loss')
+                            plt.xlabel('Epoch')
+                            plt.legend(['Train', 'Test'], loc='upper left')
+                            plt.savefig(os.path.join(save_dir, str(epochs) + 'epochs_loss_values.png'))
+                            # plt.show()
 
-                        # Plot training & validation loss values
-                        plt.plot(history.history['loss'])
-                        plt.plot(history.history['val_loss'])
-                        plt.title('Model loss')
-                        plt.ylabel('Loss')
-                        plt.xlabel('Epoch')
-                        plt.legend(['Train', 'Test'], loc='upper left')
-                        plt.savefig(os.path.join(save_dir, str(epochs) + 'epochs_loss_values.png'))
-                        # plt.show()
+                            plt.close()
 
-                        plt.close()
+                            mask_prediction = []
+                            for i in test_images:
+                                print(i.shape, "mask_prediction")
+                                mask_prediction.append(model.predict(i, verbose=1))
 
-                        mask_prediction = []
-                        for i in test_images:
-                            i = np.expand_dims(i, 0)
-                            mask_prediction.append(model.predict(i, verbose=0))
+                            rounded_pred = threshold(mask_prediction, 0.5, 0.5)
+                            rounded_pred = np.squeeze(rounded_pred, 0)
+                            mask_prediction = np.array(mask_prediction)
+                            mask_prediction = np.squeeze(mask_prediction, 0)
 
-                        rounded_pred = threshold(mask_prediction, 0.5, 0.5)
+                            print(test_images[0].shape)
+                            print(test_masks[0].shape)
+                            print(rounded_pred.shape)
+                            print(mask_prediction.shape, "mask_prediction2")
 
-                        mask_prediction = np.array(mask_prediction)
-                        mask_prediction = np.squeeze(mask_prediction, 1)
-                        np.save(os.path.join(save_dir, str(epochs) + 'epochs_mask_prediction'), mask_prediction)
-                        np.save(os.path.join(save_dir, str(epochs) + 'test_images'), test_images)
-                        np.save(os.path.join(save_dir, str(epochs) + 'test_masks'), test_masks)
-                        np.save(os.path.join(save_dir, str(epochs) + 'rounded_mask_pred'), rounded_pred)
+                            np.save(os.path.join(save_dir, str(epochs) + 'epochs_mask_prediction'), mask_prediction)
+                            np.save(os.path.join(save_dir, str(epochs) + 'test_images'), test_images)
+                            np.save(os.path.join(save_dir, str(epochs) + 'test_masks'), test_masks)
+                            np.save(os.path.join(save_dir, str(epochs) + 'rounded_mask_pred'), rounded_pred)
 
-                        results = {
-                            "epochs": epochs,
-                            "median_dice_score": "median_dice_score",
-                            "median_matthew_coeff": "median_matthew_coeff",
-                            "train_val_test_split": split,
-                            "unet_layers": layers,
-                            "data_perc": perc*100,
-                            "seed": seed,
-                            "filters": filters,
-                            "loss_function": loss_func,
-                            "patient_perc": patient_perc
-                        }
-                        dice = []
-                        m_coeffs = []
-                        output = np.squeeze(rounded_pred)
-                        test_masks_temp = np.squeeze(test_masks)
-                        for s in range(test_masks.shape[0]):
-                            dice.append(getdicescore(test_masks_temp[s, :, :], output[s, :, :]))
-                            #m_coeffs.append(matthews_coeff(test_masks_temp[s, :, :], output[s, :, :]))
+                            results = {
+                                "epochs": epochs,
+                                "median_dice_score": "median_dice_score",
+                                "median_matthew_coeff": "median_matthew_coeff",
+                                "train_val_test_split": split,
+                                "unet_layers": layers,
+                                "data_perc": perc*100,
+                                "seed": seed,
+                                "filters": filters,
+                                "loss_function": loss_func,
+                                "patient_perc": patient_perc
+                            }
+                            dice = []
+                            m_coeffs = []
+                            for i in range(len(test_masks)):
+                                for s in range(test_masks[i].shape[0]):
+                                    dice.append(getdicescore(test_masks[i][s, :, :, 0], rounded_pred[s, :, :, 0]))
+                                    #m_coeffs.append(matthews_coeff(test_masks_temp[s, :, :], output[s, :, :]))
 
-                        median_dice_score = np.median(dice)
-                        median_matthew_coeff = np.median(m_coeffs)
+                            median_dice_score = np.median(dice)
+                            median_matthew_coeff = np.median(m_coeffs)
 
-                        results["median_dice_score"] = median_dice_score
-                        results["median_matthew_coeff"] = median_matthew_coeff
+                            results["median_dice_score"] = median_dice_score
+                            results["median_matthew_coeff"] = median_matthew_coeff
 
-                        results_file = open(save_dir + "/results.txt", "w+")
-                        results_file.write(str(results))
-                        results_file.close()
+                            results_file = open(save_dir + "/results.txt", "w+")
+                            results_file.write(str(results))
+                            results_file.close()
 
-                        output = np.expand_dims(output, -1)
-                        #save_visualisation(test_images, test_masks, mask_prediction, output, median_matthew_coeff, 'matthews', save_dir)
-                        save_visualisation(test_images, test_masks, mask_prediction, output, median_dice_score, 'dice', save_dir)
-                        all_results.append(results)
+                            all_test_imgs = []
+                            all_test_masks = []
+                            for i in range(len(test_images)):
+                                for slice in test_images[i]:
+                                    all_test_imgs.append(slice)
+                                for slice in test_masks[i]:
+                                    all_test_masks.append(slice)
+                            all_test_imgs = np.array(all_test_imgs, dtype=float)
+                            all_test_masks = np.array(all_test_masks, dtype=float)
+
+                            # save_visualisation(test_images, test_masks, mask_prediction, output, median_matthew_coeff, 'matthews', save_dir)
+                            save_visualisation(all_test_imgs, all_test_masks, mask_prediction, rounded_pred, median_dice_score, 'dice', save_dir)
+                            all_results.append(results)
 
 median_matthew_coeffs = []
 best_idx = np.argmax([dict["median_matthew_coeff"] for dict in all_results])
